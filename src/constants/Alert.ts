93ccd06e3befc91d087c55fe2ead7ad63f447213
Alex Gill <alexmx2139@gmail.com>
2024-07-01 23:24:01 -0400

    Various fixes

---
+++ src/constants/Alert.ts
@@ -0,0 +1,67 @@
+import { Audio } from "expo-av";
+
+const dir = "../../assets/alerts/";
+
+const telephone_file = require(dir + "telephone.mp3");
+const chimes_up_file = require(dir + "chimes_up.mp3");
+const chimes_down_file = require(dir + "chimes_down.mp3");
+const whistle_file = require(dir + "whistle.mp3");
+const woodblock_file = require(dir + "woodblock.mp3");
+const xylophone_file = require(dir + "xylophone.mp3");
+const ocarina_file = require(dir + "ocarina.mp3");
+const grandfather_1_file = require(dir + "grandfather_clock1.mp3");
+const grandfather_2_file = require(dir + "grandfather_clock2.mp3");
+const ding_file = require(dir + "ding.mp3");
+const ding_dong_file = require(dir + "ding_dong.mp3");
+const dong_ding_file = require(dir + "dong_ding.mp3");
+const cymbal_file = require(dir + "cymbal.mp3");
+const apito_file = require(dir + "apito.mp3");
+const chords_file = require(dir + "chords.mp3");
+const obatala_file = require(dir + "obatala.mp3");
+const brave_wind_file = require(dir + "brave_wind.mp3");
+const omomuki_file = require(dir + "omomuki.mp3");
+
+export default class Alert {
+
+	static values: Alert[] = [];
+	static map: {[id: string]: Alert} = {};
+
+	static TELEPHONE = new Alert("Telephone", telephone_file);
+	static CHIMES_UP = new Alert("Chimes Up", chimes_up_file);
+	static CHIMES_DOWN = new Alert("Chimes Down", chimes_down_file);
+	static WHISTLE = new Alert("Whistle", whistle_file);
+	static WOODBLOCK = new Alert("Woodblock", woodblock_file);
+	static XYLOPHONE = new Alert("Xylophone", xylophone_file);
+	static OCARINA = new Alert("Ocarina", ocarina_file);
+	static GRANDFATHER_1 = new Alert("Grandfather 1", grandfather_1_file);
+	static GRANDFATHER_2 = new Alert("Grandfather 2", grandfather_2_file);
+	static DING = new Alert("Ding", ding_file);
+	static DING_DONG = new Alert("Ding Dong", ding_dong_file);
+	static DONG_DING = new Alert("Dong Ding", dong_ding_file);
+	static CYMBAL = new Alert("Cymbal", cymbal_file);
+	static APITO = new Alert("Apito", apito_file);
+	static CHORDS = new Alert("Chords", chords_file);
+	static OBATALA = new Alert("Obatala", obatala_file);
+	static BRAVE_WIND = new Alert("Brave Wind", brave_wind_file);
+	static OMOMUKI = new Alert("Omomuki", omomuki_file);
+	static SILENT = new Alert("Silent");
+
+	static sound: Audio.Sound = new Audio.Sound();
+
+  name: string;
+  file: any;
+
+  private constructor(name: string, file?: any) {
+    this.name = name;
+    this.file = file ? file : undefined;
+		Alert.values.push(this);
+		Alert.map[name] = this;
+  }
+	
+  async play() {
+    if (!this.file) return;
+    await Alert.sound.unloadAsync();
+    await Alert.sound.loadAsync(this.file);
+    await Alert.sound.playAsync();
+  }
+}
---
+++ src/utils/Styles.ts
@@ -0,0 +1,161 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 12,
+    gap: 10,
+  },
+  controlGroup: {
+    flex: 0,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    paddingHorizontal: 12,
+    gap: 12,
+  },
+  spaced: {
+    justifyContent: "space-between",
+  },
+  intervalList: {
+    width: "100%",
+  },
+  separator: {
+    height: 24
+  },
+  intervalBoxContainer: {
+    flex: 0,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  intervalBox: {
+    flex: 0,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+    padding: 5,
+    borderRadius: 3,
+    backgroundColor: "#00000005",
+  },
+  intervalBoxComponent: {
+    flex: 0,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 3,
+  },
+  intervalBoxNumber: {
+    backgroundColor: "#ffffff",
+    paddingVertical: 5,
+    width: 35,
+    textAlign: "center",
+    borderRadius: 5,
+  },
+  intervalEditorBoxContainer: {
+    flex: 0,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    gap: 15,
+  },
+  intervalEditorBoxModal: {
+    flex: 0,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#ffffff",
+    width: "80%",
+    borderRadius: 5,
+    alignSelf: "center",
+    marginVertical: "auto",
+    gap: 15,
+    padding: 15
+  },
+  presetModal: {
+    flex: 0,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  presetItem: {
+    flex: 0,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 15,
+  },
+  presetEditorBoxModal: {
+    flex: 0,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#ffffff",
+    width: "90%",
+    borderRadius: 5,
+    alignSelf: "center",
+    marginVertical: "auto",
+    gap: 15,
+    padding: 15
+  },
+  dropdown: {
+    backgroundColor: "#ffffff",
+    borderColor: "#575757",
+    borderWidth: 1,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
+  alertDropdown: {
+    width: 150
+  },
+  presetDropdown: {
+    width: 180
+  },
+  modalityDropdown: {
+    width: 120
+  },
+  dropdownDisabled: {
+    backgroundColor: "#bbbbbb",
+    borderColor: "#969696",
+  },
+  dropdownTextDisabled: {
+    color: "#969696",
+  },
+  picker: {
+    width: 50
+  },
+  checkBoxContainer: {
+    flex: 0,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
+  },
+  analogDisplay: {
+    width: "75%",
+    height: "75%",
+  },
+  colorPicker: {
+    flex: 1,
+    width: "100%",
+  },
+  colorPickerContainer: {
+    flex: 0,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
+    height: "60%",
+    backgroundColor: "#ffffff",
+    borderRadius: 5,
+    alignSelf: "center",
+    marginVertical: "auto",
+    gap: 40,
+    padding: 15
+  }
+});
